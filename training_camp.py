70107785688b30031009950d48e813b28fa014d6
emil-ferrologic <55745503+emil-ferrologic@users.noreply.github.com>
2024-09-23 16:06:27 +0200

    funkade inte med att hämta antal anmälda

--- training_camp.py
+++ training_camp.py
@@ -36,14 +36,14 @@ if st.session_state.state == 'ongoing':
 
     with st.form("update_report"):
 
-        sql_stmt_no_participanst = f"""SELECT count(*) as antal  from participants""";
+        # sql_stmt_no_participanst = f"""SELECT count(*) as antal  from participants""";
         # where 
         # resp_name = '{resp_name}' and 
         # resp_mail = '{resp_mail}' and
         # resp_telefon = '{resp_telefon}'
         # ; """
-        st.session_state.no_of_participants = conn.query(sql_stmt_no_participanst, ttl=600).values.tolist()[0][0]
-        print(st.session_state.no_participanst)
+        # st.session_state.no_of_participants = conn.query(sql_stmt_no_participanst, ttl=600).values.tolist()[0][0]
+        # print(st.session_state.no_participanst)
 
         # Write directly to the app
         st.title("Anmälan till Skogsluffarnas Träningsläger i Orsa 2025")
